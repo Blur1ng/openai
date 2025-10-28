aff5607eca9288593de74bc45f84c27519d57da2
Evgene <evgene@MacBook-Air-Evgene.local>
2025-10-28 15:49:40 +0300

    update db sql

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,17 @@
 \connect prompt_db;
 GRANT ALL PRIVILEGES ON DATABASE prompt_db TO postgres;
 CREATE TABLE prompt_data (
-    id           BIGSERIAL SERIAL PRIMARY KEY,
+    id           BIGSERIAL PRIMARY KEY,
     ai_model     TEXT NOT NULL,
-    prompt_name  TEXT NOT NULL UNIQUE,
+    prompt_name  TEXT NOT NULL,
     prompt       TEXT NOT NULL,
+    request      TEXT NOT NULL,
+    model        TEXT NOT NULL
+);
+
+CREATE TABLE request_data (
+    id           BIGSERIAL PRIMARY KEY,
+    ai_model     TEXT NOT NULL,
     request      TEXT NOT NULL,
     model        TEXT NOT NULL
 );
